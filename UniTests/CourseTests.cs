7579ef16ce5a83538e8e033950e20ca3dcf9b7d3
evenvagen <59914225+evenvagen@users.noreply.github.com>
2020-07-06 09:46:03 +0200

    x

---
+++ UniTests/CourseTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using MyUniversity;
+
+namespace UniTests
+{
+    public class CourseTest
+    {
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [Test]
+        public void OneCourse()
+        {
+            var course = new Course(101, "Fysikk", 4);
+            var actual = course.CourseInfo();
+            var expected = "101: Fysikk, deltagere: 4\n\n";
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+}
